2e915f679c7d1af61b66a74dd825d90dadbc89d3
jaime <jfs_x@hotmail.com>
2018-04-22 19:59:16 +0200

    Ejercicio modificado

--- xml-parser-barrapunto.py
+++ xml-parser-barrapunto.py
@@ -28,12 +28,12 @@ class myContentHandler(ContentHandler):
             self.inItem = False
         elif self.inItem:
             if name == 'title':
-                line = "Title: " + self.theContent + "."
-                file.write('<li>' + str(line) + '</li>' + '\n') 
+                line = "--->" + self.theContent + ": "
+                barrapunto.write('<li><h2>' + line + '</h2></li>') 
                 self.inContent = False
                 self.theContent = ""
             elif name == 'link':
-                file.write('<li><a href =' + self.theContent + '>' + str(self.theContent) + '</a></li>') 
+                barrapunto.write('<li><a href =' + self.theContent + '>' + str(self.theContent) + '</a></li>') 
                 self.inContent = False
                 self.theContent = ""
 
@@ -44,15 +44,16 @@ class myContentHandler(ContentHandler):
 # --- Main prog
 
 # Load parser and driver
+barrapunto = open('barrapunto.html','w')
 
-file = open('barrapunto.html','w')
 #La solución de los caracteres especiales la saque de aquí:
 #https://www.gestiweb.com/?q=content/problemas-html-acentos-y-e%C3%B1es-charset-utf-8-iso-8859-1
 opening_lines = '<!DOCTYPE html>' + '\n' + '<html>' + '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' + '\n'
 opening_lines  += '<head>' + '\n' + '<title>/.Titulares</title>' + '\n' + '</head>' + '\n'
 opening_lines +=  '<h1>Titulares de Barrapunto: </h1>' + '\n'
-opening_lines  += '<body>' + '\n' + '<ul>'
-file.write(opening_lines)
+opening_lines  += '<body>' + '\n'
+
+barrapunto.write(opening_lines)
 
 theParser = make_parser()
 theHandler = myContentHandler()
@@ -63,7 +64,7 @@ theParser.setContentHandler(theHandler)
 xmlFile = urllib.request.urlopen('http://barrapunto.com/index.rss')
 theParser.parse(xmlFile)
 
-closing_lines = '</ul>'+ '\n'+ '</body>' + '\n' + '</html>'+ '\n'
-file.write(closing_lines)
+closing_lines = '\n'+ '</body>' + '\n' + '</html>'+ '\n'
+barrapunto.write(closing_lines)
 print ("Parse complete")
 print("HTML file created")
